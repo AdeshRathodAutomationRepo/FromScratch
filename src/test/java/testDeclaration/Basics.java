84902ead256508ed01182a579b7ac09a755060c4
Adesh <adeshrathod989@gmail.com>
2024-05-22 20:22:37 +0530

    adding new

---
+++ src/test/java/testDeclaration/Basics.java
@@ -0,0 +1,29 @@
+package testDeclaration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Basics {
+	WebDriver driver;
+
+	@Test
+	public void launchBrowser() {
+
+		// initiate browser
+		driver = new ChromeDriver();
+
+		// open url
+		driver.get("https://www.amazon.in/");
+
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		// close current window
+		driver.close();
+	}
+
+}
